b13df4a858939ba78c4ff22e38be7f6eeca1c393
Carson Johnson <carson74johnson@gmail.com>
2021-11-20 16:54:20 -0500

    added a transaction variable if the if statement that opens another transaction to BudgetStore with the ability to read and write

--- public/db.js
+++ public/db.js
@@ -50,7 +50,8 @@ function checkDatabase() {
                 .then((res) => {
                     // If our returned response is not empty
                     if (res.length !== 0) {
-
+                        // Open another transaction to BudgetStore with the ability to read and write
+                        transaction = db.transaction(['BudgetStore'], 'readwrite');
                     }
                 })
         }
