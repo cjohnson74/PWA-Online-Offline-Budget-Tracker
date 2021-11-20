97e06a8793b9c6d06a629efbd8e9776260821980
Carson Johnson <carson74johnson@gmail.com>
2021-11-20 16:27:13 -0500

    added console log that says when the version is updated from the old version to the new one

--- public/db.js
+++ public/db.js
@@ -9,4 +9,7 @@ request.onupgradeneeded = function (e) {
 
     const { oldVersion } = e;
     const newVersion = e.newVersion || db.version;
+
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+    
 }
