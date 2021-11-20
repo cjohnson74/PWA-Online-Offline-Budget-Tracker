273bbe9badb275407259e966b6ca35bbb335542a
Carson Johnson <carson74johnson@gmail.com>
2021-11-20 16:29:30 -0500

    added functionality that if the db's objectStoreNames length is 0 create an objectstore names BudgetStore that autoIncrements

--- public/db.js
+++ public/db.js
@@ -11,5 +11,10 @@ request.onupgradeneeded = function (e) {
     const newVersion = e.newVersion || db.version;
 
     console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-    
+
+    db = e.target.result;
+
+    if (db.objectStoreNames.length === 0) {
+        db.createObjectStore('BudgetStore', { autoIncrement: true });
+    }
 }
