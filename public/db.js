303692f6c08795ecaa6815f2fa88b52ce3b6bfc3
Carson Johnson <carson74johnson@gmail.com>
2021-11-20 17:13:34 -0500

    added a store const that accesses the BudgetStore object

--- public/db.js
+++ public/db.js
@@ -81,4 +81,7 @@ const saveRecord = (record) => {
 
     // Create a transaction on the BudgetStore db with readwrite access
     const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    // Access your BudgetStore object store
+    const store = transaction.objectStore('BudgetStore');
 }
