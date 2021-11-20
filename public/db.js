8e3d5fbe7ab4ba7a097db0581839584caffbc75d
Carson Johnson <carson74johnson@gmail.com>
2021-11-20 16:25:22 -0500

    console log when the IndexDB is behind, create oldVersion const and new version const

--- public/db.js
+++ public/db.js
@@ -5,5 +5,8 @@ let budgetVersion;
 const request = indexDB.open('BudgetDB', budgetVersion || 21);
 
 request.onupgradeneeded = function (e) {
-    
+    console.log('Upgrade needed in IndexDB');
+
+    const { oldVersion } = e;
+    const newVersion = e.newVersion || db.version;
 }
